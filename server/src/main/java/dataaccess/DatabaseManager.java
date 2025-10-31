40ac338e112c2d260d57a4ceb79e47cdb7b85e27
dsynderg <dtsyndergaard@gmail.com>
2025-10-31 13:52:23 -0600

    authdatabases now throw exception instead of catching it

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -74,20 +74,17 @@ public class DatabaseManager {
         var port = Integer.parseInt(props.getProperty("db.port"));
         connectionUrl = String.format("jdbc:mysql://%s:%d", host, port);
     }
-    static boolean inDatabaseHelper(String gameName, String checkSql) {
-        try(Connection conn = DatabaseManager.getConnection()){
-            var statement = conn.prepareStatement(checkSql);
-            statement.setString(1,gameName);
-            var rs = statement.executeQuery();
-            if(rs.next()){
-                return true;
-            }
-            else{
-                return false;
-            }
+    static boolean inDatabaseHelper(String gameName, String checkSql) throws DataAccessException, SQLException {
+        Connection conn = DatabaseManager.getConnection();
+        var statement = conn.prepareStatement(checkSql);
+        statement.setString(1,gameName);
+        var rs = statement.executeQuery();
+        if(rs.next()){
+            return true;
         }
-        catch(SQLException | DataAccessException e){
-            throw new RuntimeException("There was a database connection issue",e);
+        else{
+            return false;
         }
+
     }
 }
--- server/src/main/java/dataaccess/SQLAuthDatabase.java
+++ server/src/main/java/dataaccess/SQLAuthDatabase.java
@@ -8,92 +8,76 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SQLAuthDatabase  {
-    public static String getUsername(String authToken){
+    public static String getUsername(String authToken) throws DataAccessException, SQLException {
         String checkSql = "SELECT username FROM AuthData WHERE authToken = ?";
-        try(Connection conn = DatabaseManager.getConnection()){
+        Connection conn = DatabaseManager.getConnection();
         var statement = conn.prepareStatement(checkSql);
         statement.setString(1,authToken);
         var response = statement.executeQuery();
         if(response.next()){
             return response.getString("username");
         }
-        }
-        catch(SQLException|DataAccessException e){
-            throw new RuntimeException("There was a database connection issue",e);
-        }
-
         return null;
+
     }
 
 
 
 
-    public static boolean addToDatabase(AuthData addObject) {
+    public static boolean addToDatabase(AuthData addObject) throws DataAccessException, SQLException {
         String query = "INSERT INTO AuthData (authToken, username) VALUES (?, ?)";
         if(inDatabase(addObject.authToken())){
             return false;
         }
-        try(Connection conn = DatabaseManager.getConnection()){
-            PreparedStatement statement = conn.prepareStatement(query);
-            statement.setString(1,addObject.authToken());
-            statement.setString(2,addObject.username());
-            statement.executeUpdate();
-            return true;
-        }
-        catch(SQLException|DataAccessException e){
-            throw new RuntimeException("There was a database connection issue",e);
-        }
+        Connection conn = DatabaseManager.getConnection();
+        PreparedStatement statement = conn.prepareStatement(query);
+        statement.setString(1,addObject.authToken());
+        statement.setString(2,addObject.username());
+        statement.executeUpdate();
+        return true;
+
 
     }
 
 
-    public static boolean removeFromDatabase(AuthData removeObject) {
+    public static boolean removeFromDatabase(AuthData removeObject) throws DataAccessException, SQLException {
         String deleteStatement = "DELETE FROM AuthData WHERE authToken = ?;";
         if(!inDatabase(removeObject.authToken())){
             return false;
         }
-        try(Connection conn = DatabaseManager.getConnection()){
-            var statement = conn.prepareStatement(deleteStatement);
-            statement.setString(1,removeObject.authToken());
-            statement.executeUpdate();
-            return true;
+        Connection conn = DatabaseManager.getConnection();
+        var statement = conn.prepareStatement(deleteStatement);
+        statement.setString(1,removeObject.authToken());
+        statement.executeUpdate();
+        return true;
 
-        }
-        catch(SQLException|DataAccessException e){
-            throw new RuntimeException("There was a database connection issue",e);
-        }
 
     }
 
 
-    public static boolean deleteall() {
+    public static boolean deleteall() throws SQLException, DataAccessException {
         String query =  "DELETE FROM authdata;";
-        try (Connection conn = DatabaseManager.getConnection()) {
-            Statement statement = conn.createStatement();
+        Connection conn = DatabaseManager.getConnection();
+        Statement statement = conn.createStatement();
 
-            statement.executeUpdate(query);
+        statement.executeUpdate(query);
 
-            return true;
-        } catch (SQLException| DataAccessException e) {
-            throw new RuntimeException("Failed to get a connection", e);
-        }
+        return true;
+
     }
 
-    public static void listDatabase(){
-        try(Connection conn = DatabaseManager.getConnection()){
-            var statement = conn.prepareStatement("select * from authdata;");
-            var response = statement.executeQuery();
-            while(response.next()){
-                String auth = response.getString("authToken");
-                String username = response.getString("username");
-                System.out.println("authToken: "+auth+", username: "+username);
-            }
+    public static void listDatabase() throws DataAccessException, SQLException {
+        Connection conn = DatabaseManager.getConnection();
+        var statement = conn.prepareStatement("select * from authdata;");
+        var response = statement.executeQuery();
+        while(response.next()){
+            String auth = response.getString("authToken");
+            String username = response.getString("username");
+            System.out.println("authToken: "+auth+", username: "+username);
         }
-        catch(SQLException|DataAccessException e){
-            throw new RuntimeException("There was a database connection issue",e);
-        }
+
     }
-    public static boolean inDatabase(String authToken){
+    public static boolean inDatabase(String authToken) throws SQLException, DataAccessException {
         String checkSql = "SELECT 1 FROM AuthData WHERE authToken = ?";
 
         return DatabaseManager.inDatabaseHelper(authToken, checkSql);
