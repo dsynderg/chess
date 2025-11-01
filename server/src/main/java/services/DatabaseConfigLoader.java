9a0cf8bc870864febd619f129b31d1383fc571f4
dsynderg <dtsyndergaard@gmail.com>
2025-10-31 22:35:18 -0600

    memory and SQL implementations are used in tandom

--- server/src/main/java/services/DatabaseConfigLoader.java
+++ server/src/main/java/services/DatabaseConfigLoader.java
@@ -4,15 +4,15 @@ import dataaccess.SQLTableControler;
 
 //allows you to ch
 public class DatabaseConfigLoader {
-    private static boolean mI = false;
+    private static boolean mI = true;
     public static boolean memoryimplementation() throws DataAccessException {
-        if(!mI){
+
             try {
                 SQLTableControler.initialize();
             } catch (DataAccessException e) {
                 throw new DataAccessException("database problem",e);
             }
-        }
+
 
         return mI;
 
