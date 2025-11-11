a402c9d33f29d01b2618e6c7deb30eff380e4093
dsynderg <dtsyndergaard@gmail.com>
2025-11-11 13:07:27 -0700

    list game works in its own function

--- client/src/main/java/services/HttpHelper.java
+++ client/src/main/java/services/HttpHelper.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.BeforeAll;
 import server.Server;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -21,6 +23,25 @@ public class HttpHelper {
 
     public static int port = 8080;
 //    private static final Server server = new Server();
+    private void listgames(HttpClient client, String fullpath, AuthData data) throws Exception {
+        Gson gson = new Gson();
+        var response = send(client, HttpRequest.newBuilder()
+                .uri(new URI(fullpath))
+                .GET()
+                .header("authorization", data.authToken())
+                .build());
+        if (response != null) {
+            System.out.println("If no player is shown in a game, that means that the seat is open");
+            var responsemap = gson.fromJson(response.body(), Map.class);
+            var responselist = responsemap.get("games");
+//                    System.out.println(responselist);
+            //
+            for (var game : (Iterable<?>) responselist) System.out.println(game);
+        }
+        else {
+            System.out.println("There were no games to be listed");
+        }
+    }
 
     public HttpResponse<String> requestMaker(RequestType type, String path, String json, AuthData data) throws Exception {
 
@@ -32,18 +53,8 @@ public class HttpHelper {
             String fullpath = "http://localhost:"+port+"/" + path;
             if (type == RequestType.get) {
                 if(Objects.equals(path,"game")){
-                    Gson gson = new Gson();
-                    var response = send(client, HttpRequest.newBuilder()
-                            .uri(new URI(fullpath))
-                            .GET()
-                            .header("authorization", data.authToken())
-                            .build());
-                    System.out.println("games were listed");
-                    var responsemap = gson.fromJson(response.body(), Map.class);
-                    var responselist =  responsemap.get("games");
-//                    System.out.println(responselist);
-                    //
-                    for(var game: (Iterable<?>) responselist) System.out.println(game);
+                    listgames(client,fullpath,data);
+
                 }
 
             }
